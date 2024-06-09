486080f5ee476867ff8765748f773c8f4b415a3d
Breno Guilherme de Souza Lopes <105818141+brenogdev@users.noreply.github.com>
2024-06-08 22:05:34 -0300

    chore: setup

---
+++ app/doctor/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function DoctorIdPage({
+  params,
+}: Readonly<{ params: { id: string } }>) {
+  return <div>page</div>;
+}
---
+++ app/doctor/page.tsx
@@ -0,0 +1,8 @@
+ import React from 'react'
+ 
+ export default function DoctorPage() {
+   return (
+     <div>page</div>
+   )
+ }
+ 
